ab9ac27ce337c97ecb1151ff59f266e21f313319
Dmitry Sky <dmitry.sky@icloud.com>
2015-12-19 14:56:41 -0700

    delete skill from the database

--- app/assets/javascripts/components/_all_skills.js.jsx
+++ app/assets/javascripts/components/_all_skills.js.jsx
@@ -1,11 +1,16 @@
 var AllSkills = React.createClass({
+  handleDelete(id) {
+    this.props.handleDelete(id);
+  },
+
   render() {
-    var skills = this.props.skills.map((skill, index) => {
+    let skills = this.props.skills.map((skill, index) => {
       return (
         <div key={index}>
           <h3>{skill.name}</h3>
           <p><strong>Level:</strong> {skill.level}</p>
           <p>{skill.details}</p>
+          <button onClick={this.handleDelete.bind(this, skill.id)}>Delete</button>
         </div>
       )
     });
